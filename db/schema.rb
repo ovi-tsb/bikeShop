c985f24495c6d7f26716449f0e29c89350135445
Ovi Turcu <ovi_tsb@yahoo.com>
2017-03-23 09:33:23 -0400

    added price and color to products

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170321153632) do
+ActiveRecord::Schema.define(version: 20170322161602) do
 
   create_table "products", force: :cascade do |t|
     t.string   "name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20170321153632) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.string   "colour"
+    t.decimal  "price"
   end
 
 end
