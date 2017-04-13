0df872386253279431752af5bd8062e31fe03e28
Ovi Turcu <ovi_tsb@yahoo.com>
2017-04-13 01:13:01 -0400

    5.1 task

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,7 +4,12 @@ class ProductsController < ApplicationController
   # GET /products
   # GET /products.json
   def index
+    if params[:q]
+    search_term = params[:q]
+    @products = Product.search(search_term)
+    else
     @products = Product.all
+    end
   end
 
   # GET /products/1
@@ -28,7 +33,7 @@ class ProductsController < ApplicationController
 
     respond_to do |format|
       if @product.save
-        format.html { redirect_to @product, notice: 'Product was successfully created.' }
+        format.html { redirect_to "/products", notice: 'Product was successfully created.' }
         format.json { render :show, status: :created, location: @product }
       else
         format.html { render :new }
