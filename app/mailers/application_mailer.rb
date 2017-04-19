fa3321f2db82a21091bb9dfec8babd37410c668c
Ovi Turcu <ovi_tsb@yahoo.com>
2017-04-19 01:21:53 -0400

    SendGrid

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,15 @@
 class ApplicationMailer < ActionMailer::Base
   default from: 'from@example.com'
   layout 'mailer'
+
+  def thank_you
+    @name = params[:name]
+    @email = params[:email]
+    @message = params[:message]
+    ActionMailer::Base.mail(:from => @email,
+        :to => 'ovi_tsb@yahoo.com',
+        :subject => "A new contact form message from #{@name}",
+        :body => @message).deliver_now
+  end
+
 end
