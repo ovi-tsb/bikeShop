ff39f8fa92fe8d051afafbc91e7d3985ef119308
Ovi Turcu <ovi_tsb@yahoo.com>
2017-05-04 13:11:40 -0400

    Authorization CanCanCan

---
+++ app/assets/javascripts/alert.js
@@ -0,0 +1,3 @@
+$(document).on('turbolinks:load', function() {
+  $(".alert, .notice").delay(3000).fadeOut(1000);
+})
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users,  :path => '', 
                 :path_names => {:sign_in => 'login', 
                   :sign_out => 'logout'}
+  
   resources :users
   resources :products
   get 'static_pages/about'
