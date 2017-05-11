8d4816d06ebad1d19bc5bbf035720de32708a28b
Ovi Turcu <ovi_tsb@yahoo.com>
2017-05-10 20:47:48 -0400

    routes.rb rollback

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,7 @@
 Rails.application.routes.draw do
-  #devise_for :users,  :path => '', :path_names => {:sign_in => 'login', :sign_out => 'logout'}
+  devise_for :users,  :path => '', :path_names => {:sign_in => 'login', :sign_out => 'logout'}
   
-  devise_scope :user do
-      get 'login', to: 'devise/sessions#new'
-      get 'register', to: 'devise/registrations#new', as: "new_user_registration"
-      delete 'logout', to: 'devise/sessions#destroy'
-    end
-
-    # Below for all other routes:
-    devise_for :users, :controllers => { registrations: 'registrations' }
-
-       
+  
   resources :users
   resources :products
 
