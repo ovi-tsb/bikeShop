b3d0c78361ab1ac3f61324b62f9a0dbb4286c439
Ovi Turcu <ovi_tsb@yahoo.com>
2017-05-23 12:15:33 -0400

    mailer and search  modif at 5.11

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,7 @@ class ApplicationController < ActionController::Base
     redirect_to main_app.root_url, :alert => exception.message
   end
 
+# adding field first_name  last_name to devise
   before_action :configure_permitted_parameters, if: :devise_controller?
 
     protected
@@ -13,4 +14,6 @@ class ApplicationController < ActionController::Base
     def configure_permitted_parameters
       devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
     end
+# adding field first_name  last_name to devise
+    
 end
