15c8a59bcdb5e94dcbf3a5953d1c615dc5b240ff
Ovi Turcu <ovi_tsb@yahoo.com>
2017-06-04 23:57:46 -0400

    6.6 creating payments

---
+++ app/controllers/payments_controller.rb
@@ -0,0 +1,29 @@
+class PaymentsController < ApplicationController
+  def create
+    @product = Product.find(params[:product_id])
+    @user = current_user
+    token = params[:stripeToken]
+      # Create the charge on Stripe's servers - this will charge the user's card
+      begin
+        charge = Stripe::Charge.create(
+          amount: (@product.price*100).to_i, # amount in cents, again
+          currency: "usd",
+          source: token,
+          receipt_email: (@user.email),
+          description: params[:stripeEmail]
+        )
+
+        if charge.paid
+          Order.create(product_id: @product.id,user_id: @user.id, total: @product.price ) 
+          flash[:notice] = "Your payment was processed successfully" 
+        end
+
+      rescue Stripe::CardError => e
+        # The card has been declined
+        body = e.json_body
+        err = body[:error]
+        flash[:error] = "Unfortunately, there was an error processing your payment: #{err[:message]}"
+      end
+      redirect_to product_path(@product)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -7,16 +7,13 @@ Rails.application.routes.draw do
     resources :comments
   end
 
-
   get 'static_pages/about'
-
   get 'static_pages/contact'
-
   get 'static_pages/index'
   #root 'static_pages#index'
   root 'static_pages#landing_page'
-
   post 'static_pages/thank_you'
+  post 'payments/create'
 
   resources :orders, only: [:index, :show, :create, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
