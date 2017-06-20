11ec9d5a5713ce3fac776ef3224042c88b4c2e69
Ovi Turcu <ovi_tsb@yahoo.com>
2017-06-20 01:18:27 -0400

    6.10 AJAX comments

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -1,12 +1,18 @@
-$(document).on('turbolinks:load', function(){
-    $('.rating').raty({ path: '/assets', scoreName: 'comment[rating]' });
-    $('.rated').raty({ path: '/assets',
+var refreshRating = function () {
+  $('.rating').raty({ path: '/assets', scoreName: 'comment[rating]' });
+  $('.rated').raty({ path: '/assets',
           readOnly: true,
           score: function() {
             return $(this).attr('data-score');
           }
         });
+};
 
+
+
+$(document).on('turbolinks:load', function(){
+    
+    refreshRating();
     //Image Constrain
     $(".img-zoom").elevateZoom({ zoomType: "lens", containLensZoom: true});
    
--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,10 +4,12 @@ class CommentsController < ApplicationController
     @product = Product.find(params[:product_id])
     @comment = @product.comments.new(comment_params)
     @comment.user = current_user
+    @user = current_user
     respond_to do |format|
           if @comment.save
             format.html { redirect_to @product, notice: 'Review was created successfully.' }
             format.json { render :show, status: :created, location: @product }
+            format.js
           else
             format.html { redirect_to @product, alert: 'Review was not saved successfully.' }
             format.json { render json: @comment.errors, status: :unprocessable_entity }
